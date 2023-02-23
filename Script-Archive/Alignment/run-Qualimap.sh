022596816cf39230748d3176ca0a66eca06b9d60
Malcolm Todd <gwy260@adenine.usask.ca>
2023-02-23 17:00:26 -0600

    Clearing deleted Script-Archive folder from status

--- Script-Archive/Alignment/run-Qualimap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bowtie2OutputFilePathPrefix=$1
-qualimapReportDirPath=$2
-
-echo "Reviewing Bowtie2 output file..."
-qualimap bamqc -bam ${bowtie2OutputFilePathPrefix}.sorted.bam -outdir $qualimapReportDirPath -outformat PDF --java-mem-size=8G
--- Script-Archive/Alignment/run-Trimmomatic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-trimmomaticFilePath=$1
-trimmomaticJARFile=$2
-shortReadPEFilePathPrefix=$3
-
-echo "Running Trimmomatic..."
-java -jar $trimmomaticFilePath/$trimmomaticJARFile PE ${shortReadPEFilePathPrefix}_1.fastq ${shortReadPEFilePathPrefix}_2.fastq ${shortReadPEFilePathPrefix}_1_Paired.fastq ${shortReadPEFilePathPrefix}_1_Unpaired.fastq ${shortReadPEFilePathPrefix}_2_Paired.fastq ${shortReadPEFilePathPrefix}_2_Unpaired.fastq ILLUMINACLIP:$trimmomaticFilePath/adapters/TruSeq3-PE.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:15 MINLEN:50
